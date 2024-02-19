399cfdea39e5a88fece99f3e64dd55cb17cb1b17
Abed Al Ghani Shaaban <abedshaaban600@gmail.com>
2024-02-19 15:01:01 +0200

    removed unused code and files

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,8 +1,6 @@
+import { cn } from "@/lib/utils";
 import "./globals.css";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Turborepo",
@@ -16,7 +14,11 @@ export default function RootLayout({
 }): JSX.Element {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={cn("bg-background min-h-screen font-sans antialiased")}>
+        <div className="bg-background relative flex min-h-screen flex-col">
+          <main className="container flex-1">{children}</main>
+        </div>
+      </body>
     </html>
   );
 }
