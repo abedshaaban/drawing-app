e08991bd8e1ef36b2c4934eabee3925c1975fa24
Abed Al Ghani Shaaban <abedshaaban600@gmail.com>
2024-02-20 11:33:58 +0200

    enabled cors and return response after register

--- apps/backend/src/auth/auth.controller.ts
+++ apps/backend/src/auth/auth.controller.ts
@@ -4,9 +4,9 @@ import { UserDTO } from './auth.dto';
 @Controller('auth')
 export class AuthController {
   @Post('register')
-  register(@Body() createCatDto: UserDTO) {
-    console.log(createCatDto);
+  register(@Body() createCatDto: UserDTO): object {
+    console.log('12', createCatDto);
 
-    return;
+    return { x: 'we hit those' };
   }
 }
